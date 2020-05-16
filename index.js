85bebeb99315c0ca569641170da1147703ee9046
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-16 03:17:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function writeCards(names,eventName) {
   for (let i = 0; i < names.length; i++) {
       newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`) 
   }
+  return newArr;
 }
 
 
