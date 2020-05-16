20c15a8fd230dfed507441488898cd16faf953d3
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-16 03:17:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // Code your solutions in this file
+function writeCards(names,eventName) {
+  const newArr  [];
+  for (let i = 0; i < names.length; i++) {
+      newArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`) 
+  }
+}
+
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
 function countDown(countDown) {
     while (countDown>=0){
         console.log(countDown);
